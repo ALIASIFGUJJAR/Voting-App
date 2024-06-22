b1998f340e0af327bbbc743b8f11a85c635fb4a4
ALIASIFGUJJAR <aliasif3324@gmail.com>
2024-06-22 18:17:28 +0200

    complete mongoose schema of Users.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,47 @@
 //User.JS
+const { request } = require('express');
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    age:{
+        type:number,
+        require:true
+
+    },
+    email:{
+        type:String,
+        
+    },
+    mobile:{
+        type:String,
+    },
+    address:{
+        type:String,
+        require:true
+    },
+    cnicNumber:{
+        type:Number,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    role:{
+        type:String,
+        enum: ['voter','admin'],
+        require: true
+    },
+    isVoted:{
+        type: Boolean,
+        default: false
+    } 
+})
+
+const user=mongoose.model('User',userSchema)
+module.exports= User;
